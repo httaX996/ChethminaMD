513ad49e8e0ca0cf44c29ca37314e76a58290cfd
Chethmina Kavishan <atadeka999@gmail.com>
2025-04-25 14:44:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ubQVlAab#GHN6FmBi03xNXWckSxLHWqczHub6ShvuukXQWYISAIw",
+SESSION_ID: process.env.SESSION_ID || "GnQFXTTa#Cobg1Lb0w0tpb0dfJLum49BK2tCyu5PuifbVpk-6Rgs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://envs.sh/8HL.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "◅          🧙🏻‍♂️𝗖𝗛𝗘𝗧𝗛𝗠𝗜𝗡𝗔 𝗠𝗗🧙🏻‍♂️          ▻\n\n*Hey there I'm alive now! 💐*\n\nʜᴇʀᴇ'ꜱ ᴡʜᴀᴛ ᴄᴀɴ ɪ ᴅᴏ👇🏻\n* *ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴏɴɢ & ᴠɪᴅᴇᴏ📥*\n* *ꜰᴇᴛᴄʜ ʟᴀᴛᴇꜱᴛ ɴᴇᴡꜱ📰*\n* *ᴍᴀɴᴀɢᴇ ɢʀᴏᴜᴘ👮🏻‍♂️*\n* *ᴅᴏᴡɴʟᴏᴀᴅ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ, ᴍᴇᴀᴅɪᴀꜰɪʀᴇ ꜰɪʟᴇꜱ🗂️*\n\n*Type _.menu_ to get full commands list 📃*\n\n\n> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*",
 MODE: process.env.MODE || "public",
